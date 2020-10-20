6533805bf94d5c2e43824577b906c4d415db9442
Jordi Simon <72779969+jordisl@users.noreply.github.com>
2020-10-20 06:16:11 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,29 @@
+let punts = 0
+let marcador = 0
 input.onButtonPressed(Button.A, function () {
     if (punts == 1) {
-        narcador = narcador + 1
+        marcador = marcador + 1
     }
 })
 input.onButtonPressed(Button.AB, function () {
     if (punts == 3) {
-        narcador = narcador + 1
+        marcador = marcador + 1
     }
 })
 input.onButtonPressed(Button.B, function () {
     if (punts == 2) {
-        narcador = narcador + 1
+        marcador = marcador + 1
     }
+})
+input.onGesture(Gesture.Shake, function () {
+    marcador = 0
+    marcador = 0
+    for (let index = 0; index < 20; index++) {
+        triaimostrapunts()
+        basic.pause(500)
+        basic.clearScreen()
+    }
+    basic.showString("" + (marcador))
 })
 function triaimostrapunts () {
     punts = randint(1, 3)
@@ -19,16 +31,6 @@ function triaimostrapunts () {
         led.plot(randint(0, 4), randint(0, 4))
     }
 }
-let punts = 0
-let narcador = 0
-narcador = 0
-narcador = 0
-for (let index = 0; index < 20; index++) {
-    triaimostrapunts()
-    basic.pause(500)
-    basic.clearScreen()
-}
-basic.showString("" + (narcador))
 basic.forever(function () {
 	
 })
